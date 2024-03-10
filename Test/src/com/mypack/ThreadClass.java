95ff3ef0b779379ec0a37c1ad825ff95d2c8292b
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-10 22:51:56 +0530

    Thread Class getName() Method.

---
+++ Test/src/com/mypack/ThreadClass.java
@@ -0,0 +1,15 @@
+package com.mypack;
+
+class ThreadDemo extends Thread{
+	public void run() {
+		
+		System.out.println("Run method executed by Thread: "+Thread.currentThread().getName());
+	}
+}
+class ThreadClass {
+	public static void main(String[] args) {
+		ThreadDemo td = new ThreadDemo();
+		td.start();
+		System.out.println("Main method executed by Thread: "+Thread.currentThread().getName());
+	}
+}
