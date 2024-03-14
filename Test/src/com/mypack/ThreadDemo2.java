c4ac827e53b5b6da32362f1b50ecaa08d2230037
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-14 22:24:45 +0530

    commit

---
+++ Test/src/com/mypack/ThreadDemo2.java
@@ -0,0 +1,16 @@
+package com.mypack;
+
+class MyThread2 extends Thread {
+	public void start() {
+		System.out.println("start() method");	
+	}
+	public void run() {
+		System.out.println("run() method");
+	}
+}
+
+public class ThreadDemo2 {
+	public static void main(String[] args) {
+		
+	}
+}
