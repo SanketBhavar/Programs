671c6c758a884a33ed5632f805f2fcfb2f65f3a3
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-14 22:35:23 +0530

    without override start method.

--- Test/src/com/mypack/ThreadDemo2.java
+++ Test/src/com/mypack/ThreadDemo2.java
@@ -11,6 +11,8 @@ class MyThread2 extends Thread {
 
 public class ThreadDemo2 {
 	public static void main(String[] args) {
-		
+		MyThread2 t = new MyThread2();
+		t.start();
+		System.out.println("main() method");
 	}
 }
