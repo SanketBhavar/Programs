7ae4d9d0708e8edcf2df2724ae2d82f69bd42920
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-14 22:44:08 +0530

    Override the start() Method

--- Test/src/com/mypack/ThreadDemo2.java
+++ Test/src/com/mypack/ThreadDemo2.java
@@ -2,6 +2,7 @@ package com.mypack;
 
 class MyThread2 extends Thread {
 	public void start() {
+		super.start();    
 		System.out.println("start() method");	
 	}
 	public void run() {
@@ -12,7 +13,22 @@ class MyThread2 extends Thread {
 public class ThreadDemo2 {
 	public static void main(String[] args) {
 		MyThread2 t = new MyThread2();
-		t.start();
+		t.start();  
+		//in this case the entire output is produced by main thread. 
+		//if we override the start method using super keyword, then child thread-run method also executed. 
 		System.out.println("main() method");
 	}
 }
+
+/*
+Output: without using super keyword
+start() method
+main() method
+*/
+
+/*
+Output: using super keyword
+start() method
+run() method
+main() method
+*/
