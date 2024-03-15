ce3abc40eb3e1179c6eabb99dcac3e2ec15e1ab0
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-15 21:36:31 +0530

    Runnable Interface

---
+++ Test/src/com/mypack/ThreadDemo3.java
@@ -0,0 +1,20 @@
+package com.mypack;
+
+class MyRunnable implements Runnable {
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println("Child Thread");
+		}
+	}
+}
+
+public class ThreadDemo3 {
+	public static void main(String[] args) {
+		MyRunnable r = new MyRunnable();
+		Thread t = new Thread(r);
+		t.start();
+		for(int i=0;i<10;i++) {
+			System.out.println("Main Thread");
+		}
+	}
+}
