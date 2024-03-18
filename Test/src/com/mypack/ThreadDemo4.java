21ff7788809d235915eb1507a88111663e0c5199
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-18 22:16:40 +0530

    Getting and Setting name of a Thread.

--- Test/src/com/mypack/ThreadDemo4.java
+++ Test/src/com/mypack/ThreadDemo4.java
@@ -7,5 +7,9 @@ class MyThread4 extends Thread {
 public class ThreadDemo4 {
 	public static void main(String[] args) {
 		System.out.println(Thread.currentThread().getName()); 
+		MyThread4 t = new MyThread4();
+		System.out.println(t.getName());
+		Thread.currentThread().setName("Sanket Thread");   //setting name for thread
+		System.out.println(Thread.currentThread().getName()); //get the name of thread
 	}
 }
