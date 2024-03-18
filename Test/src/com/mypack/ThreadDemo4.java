8b6e1006b56955c6bae3aaa8526606678b80f635
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-18 22:12:09 +0530

    getName() method.

---
+++ Test/src/com/mypack/ThreadDemo4.java
@@ -0,0 +1,11 @@
+package com.mypack;
+
+class MyThread4 extends Thread {
+	
+}
+
+public class ThreadDemo4 {
+	public static void main(String[] args) {
+		System.out.println(Thread.currentThread().getName()); 
+	}
+}
