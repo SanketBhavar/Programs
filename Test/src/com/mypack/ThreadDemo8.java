5e160a821d8e2e2e7ee8f7beb08f90a00d347089
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-22 21:05:01 +0530

    implementing ThreadDemo8 class

--- Test/src/com/mypack/ThreadDemo8.java
+++ Test/src/com/mypack/ThreadDemo8.java
@@ -2,7 +2,7 @@ package com.mypack;
 
 class MyThread8 extends Thread {
 	public void run() {
-		for (int i=0;i<10;i++) {
+		for (int i=0;i<5;i++) {
 			System.out.println("Sanket Thread");
 			try {
 				Thread.sleep(2000);
@@ -15,6 +15,10 @@ class MyThread8 extends Thread {
 
 public class ThreadDemo8 {
 	public static void main(String[] args) {
-		
+		MyThread8 t = new MyThread8();
+		t.start();
+		for(int i=0;i<5;i++) {
+			System.out.println("Rahul Thread");
+		}
 	}
 }
