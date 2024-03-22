416ee193baf3326f0809f6093b3ff6d706338823
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-22 21:11:01 +0530

    join() method.

--- Test/src/com/mypack/ThreadDemo8.java
+++ Test/src/com/mypack/ThreadDemo8.java
@@ -7,16 +7,19 @@ class MyThread8 extends Thread {
 			try {
 				Thread.sleep(2000);
 			}
-			catch (InterruptedException e) {				
+			catch (InterruptedException e) {
+				System.out.println(e);
 			}		
 		}
 	}
 }
 
 public class ThreadDemo8 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		MyThread8 t = new MyThread8();
 		t.start();
+		t.join();
+		//join() method- If a thread wants to wait until completing some other thread.
 		for(int i=0;i<5;i++) {
 			System.out.println("Rahul Thread");
 		}
