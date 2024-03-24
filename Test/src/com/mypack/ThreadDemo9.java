9c2752e8c9a2a67f20bb9b9b942530419bf87506
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-24 12:00:42 +0530

    Join() Method.

---
+++ Test/src/com/mypack/ThreadDemo9.java
@@ -0,0 +1,15 @@
+package com.mypack;
+
+public class ThreadDemo9 {
+	public static void main(String[] args) throws InterruptedException
+	{
+		System.out.println("M");
+		Thread.sleep(3000);
+		System.out.println("E");
+		Thread.sleep(3000);
+		System.out.println("G");
+		Thread.sleep(3000);
+		System.out.println("A");
+		Thread.sleep(3000);
+	}
+}
