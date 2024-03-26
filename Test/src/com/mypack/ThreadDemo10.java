71a3dd32f5508ea74c736e08e02b3a40b07e3985
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-26 22:14:47 +0530

    Implementing MyThread10 class

--- Test/src/com/mypack/ThreadDemo10.java
+++ Test/src/com/mypack/ThreadDemo10.java
@@ -3,7 +3,17 @@ package com.mypack;
 class MyThread10 extends Thread {
 	public void run()
 	{
-	
+		try {
+			for(int i=0;i<5;i++)
+			{
+				System.out.println("I am Lazy Thread");
+				Thread.sleep(2000);
+			}
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("I got Interrupted..!");
+		}
 	}
 }
 
