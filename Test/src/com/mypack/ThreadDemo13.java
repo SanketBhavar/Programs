b30c7a34444e01fe516b04effa08b12628359825
SanketBhavar <bhavarsanket2710@gmail.com>
2024-03-30 22:21:29 +0530

    commit

---
+++ Test/src/com/mypack/ThreadDemo13.java
@@ -0,0 +1,45 @@
+package com.mypack;
+
+class Display1 {
+	public void wish(String name)
+	{
+		;;;;;;;;;;;//1 Lakh lines of code.
+		for(int i=0;i<5;i++)
+		{
+			System.out.print("Good Morning: ");
+			try
+			{
+				Thread.sleep(2000);					
+			}
+			catch(InterruptedException e)
+			{
+			}
+			System.out.println(name);
+		}
+		;;;;;;;;;;;//1 Lakh lines of code.
+	}
+}
+
+class MyThread13 extends Thread {
+	Display1 d;
+	String name;
+	MyThread13(Display1 d, String name)
+	{
+		this.d=d;
+		this.name=name;
+	}
+	public void run() 
+	{
+		d.wish(name);
+	}
+}
+
+public class ThreadDemo13 {
+	public static void main(String[] args) {
+		Display1 d = new Display1();
+		MyThread13 t1 = new MyThread13(d, "Rohit");
+		MyThread13 t2 = new MyThread13(d, "Virat");
+		t1.start();
+		t2.start();
+	}
+}
