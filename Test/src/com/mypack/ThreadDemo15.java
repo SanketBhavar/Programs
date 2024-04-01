7e6e65bf55692efd1cb7ac340362989d48c4311c
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-01 21:23:55 +0530

    With wait(), notify() method.

--- Test/src/com/mypack/ThreadDemo15.java
+++ Test/src/com/mypack/ThreadDemo15.java
@@ -6,7 +6,7 @@ public class ThreadDemo15 {
 		ThreadA b = new ThreadA();
 		b.start();
 		synchronized(b) {
-			System.out.println("main Thread calling wait() method");
+			System.out.println("main Thread calling wait() method");  //Step 1
 			b.wait();
 			System.out.println("main Thread got notification call");
 			System.out.println(b.total);
@@ -18,14 +18,21 @@ class ThreadA extends Thread {
 	int total=0;
 	public void run() {
 		synchronized(this) {
-			System.out.println("child thread starts calculation");
+			System.out.println("child thread starts calculation");  //Step 2
 			for(int i=0;i<=100;i++) 
 			{
 				total=total+i;
 			}
-			System.out.println("child thread giving notification call");
+			System.out.println("child thread giving notification call");  //Step 3
 			this.notify();
 		}
 	}
 }
 
+/*
+main Thread calling wait() method
+child thread starts calculation
+child thread giving notification call
+main Thread got notification call
+5050
+ */
