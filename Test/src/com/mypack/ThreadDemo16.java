d8034a5604fa414845f65adc8f271fe799124552
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-03 22:02:08 +0530

    Implementing B class.

--- Test/src/com/mypack/ThreadDemo16.java
+++ Test/src/com/mypack/ThreadDemo16.java
@@ -12,12 +12,33 @@ class A {
 		System.out.println("Thread1 trying to call b.last()");
 		b.last();
 	}
-	public synchronized void last()
+	public void last()
 	{
 		System.out.println("inside A, this is last() method");
 	}
 }
 
+class B {
+	public synchronized void d2(A a) {
+		System.out.println("Thread2 starts execution of d2() method");
+		try
+		{
+			Thread.sleep(2000);
+		}
+		catch(InterruptedException e)
+		{ }
+		System.out.println("Thread2 trying to call A.last()");
+		a.last();
+	}
+	public void last()
+	{
+		System.out.println("inside B, this is last() method");
+	}
+}
+
 public class ThreadDemo16 implements Runnable {
 	
+	public static void main(String[] args) {
+		
+	}
 }
