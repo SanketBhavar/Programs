62f81eacb8ef6d9cfa972e1ef6c9ca0e53ccc6ec
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-06 00:32:34 +0530

    Deadloack with all methods as synchronized.

--- Test/src/com/mypack/ThreadDemo18.java
+++ Test/src/com/mypack/ThreadDemo18.java
@@ -12,7 +12,7 @@ class A2 {
 		System.out.println("Thread1 trying to call b.last()");
 		b.last();
 	}
-	public void last()
+	public synchronized void last()
 	{
 		System.out.println("inside A2, this is last() method");
 	}
@@ -30,7 +30,7 @@ class B2 {
 		System.out.println("Thread2 trying to call A.last()");
 		a.last();
 	}
-	public void last()
+	public synchronized void last()
 	{
 		System.out.println("inside B2, this is last() method");
 	}
@@ -56,5 +56,13 @@ public class ThreadDemo18 implements Runnable {
 }
 
 //Synchronized keyword is the cause for deadlock hence whenever we are using synchronized keyword we have to take special care.
-//In above program, we have using synchronized keyword for 2 methods so that deadloack is not occur in this program.
-//but In this program, output is in sync.
+//In above program, we have using synchronized keyword for 4 methods so that deadloack is occur in this program.
+//but In this program, Program is in execution state and both threads are waiting for execution of each others methods and 
+//it never terminated, this is nothing but DEADLOCK.
+
+/*
+Thread1 starts execution of d1() method
+Thread2 starts execution of d2() method
+Thread1 trying to call b.last()
+Thread2 trying to call A.last()
+*/
