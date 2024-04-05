87ec8d10a13a11c362d6b820e2c69731720c08e3
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-06 00:26:14 +0530

    Deadloack with all methods as synchronized.

---
+++ Test/src/com/mypack/ThreadDemo18.java
@@ -0,0 +1,60 @@
+package com.mypack;
+
+class A2 {
+	public synchronized void d1(B2 b) {
+		System.out.println("Thread1 starts execution of d1() method");
+		try
+		{
+			Thread.sleep(2000);
+		}
+		catch(InterruptedException e)
+		{ }
+		System.out.println("Thread1 trying to call b.last()");
+		b.last();
+	}
+	public void last()
+	{
+		System.out.println("inside A2, this is last() method");
+	}
+}
+
+class B2 {
+	public synchronized void d2(A2 a) {
+		System.out.println("Thread2 starts execution of d2() method");
+		try
+		{
+			Thread.sleep(2000);
+		}
+		catch(InterruptedException e)
+		{ }
+		System.out.println("Thread2 trying to call A.last()");
+		a.last();
+	}
+	public void last()
+	{
+		System.out.println("inside B2, this is last() method");
+	}
+}
+
+public class ThreadDemo18 implements Runnable {
+	A2 a = new A2();
+	B2 b = new B2();
+	public void m1() 
+	{
+		Thread t = new Thread(this);
+		t.start();
+		a.d1(b); 	//this line expected by main thread.
+	}
+	public void run()
+	{
+		b.d2(a);  //this line expected by child thread.
+	}
+	public static void main(String[] args) {
+		ThreadDemo18 td = new ThreadDemo18();  //main thread
+		td.m1();
+	}
+}
+
+//Synchronized keyword is the cause for deadlock hence whenever we are using synchronized keyword we have to take special care.
+//In above program, we have using synchronized keyword for 2 methods so that deadloack is not occur in this program.
+//but In this program, output is in sync.
