afe7fdc6cbc3d571bf77e7859fe2d4fdf3d8cf5c
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-10 22:02:25 +0530

    Daemon Thread.

--- Test/src/com/mypack/ThreadDemo19.java
+++ Test/src/com/mypack/ThreadDemo19.java
@@ -19,8 +19,15 @@ class MyThread19 extends Thread {
 public class ThreadDemo19 {
 	public static void main(String[] args) {
 		MyThread19 t = new MyThread19();
-		//t.setDaemon(true);
+		t.setDaemon(true); //-----> line 22
 		t.start();
 		System.out.println("end of main thread");
 	}
 }
+
+/*
+ * If we are commenting line 22 then both main and child threads are non daemon and hence both will be executed until they completion.
+ * 
+ * If we are not commenting line 22 then both main thread is non daemon and child thread is daemon and hence whenever main thread terminates 
+   automatically child thread will be terminated. 
+*/
