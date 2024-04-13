1b40a9926abf41cd96f7b9d8fe55bcf431993050
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-13 21:44:05 +0530

    Exception Handling Example.

---
+++ Test/src/com/mypack/ExceptionDemo.java
@@ -0,0 +1,13 @@
+package com.mypack;
+
+public class ExceptionDemo {
+	public static void main(String[] args) {
+		doStuff();
+	}
+	public static void doStuff() {
+		doMoreStuff();
+	}
+	public static void doMoreStuff() {
+		System.out.println("Hello");
+	}
+}
