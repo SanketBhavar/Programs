08d1e8f541a90e8f3869b67370e6ea4368b637e6
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-18 23:22:04 +0530

    Default Exception Handling in Java.

--- Test/src/com/mypack/ExceptionDemo3.java
+++ Test/src/com/mypack/ExceptionDemo3.java
@@ -7,16 +7,16 @@ public class ExceptionDemo3 {
 	}
 	public static void doStuff() {
 		doMoreStuff();
-		System.out.println("Hi.!!");
+		System.out.println("Hi..!!");
 	}
 	public static void doMoreStuff() {
-		System.out.println("Hello");
+		System.out.println("Hello..!!");
 	}
 }
 
 /*
- Hello
+Hello
+Hi.!!
 Exception in thread "main" java.lang.ArithmeticException: / by zero
-	at com.mypack.ExceptionDemo2.doStuff(ExceptionDemo2.java:9)
-	at com.mypack.ExceptionDemo2.main(ExceptionDemo2.java:5)
- */
+	at com.mypack.ExceptionDemo3.main(ExceptionDemo3.java:6)
+*/
