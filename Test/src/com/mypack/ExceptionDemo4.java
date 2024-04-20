07dec9ba1150aa738a3b08ffe54d896bc73e56da
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-20 21:59:58 +0530

    Exception Handling Without try catch.

--- Test/src/com/mypack/ExceptionDemo4.java
+++ Test/src/com/mypack/ExceptionDemo4.java
@@ -3,7 +3,13 @@ package com.mypack;
 public class ExceptionDemo4 {
 	public static void main(String[] args) {
 		System.out.println("Statement 1");
-		System.out.println(10/0);
+		System.out.println(10/0);		//Abnormal Termination.
 		System.out.println("Statement 2");
 	}
 }
+
+/*
+ Statement 1
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at com.mypack.ExceptionDemo4.main(ExceptionDemo4.java:6)
+ */
