1fb08a73c5b800250ace1d7a44344517f041e221
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-20 21:55:18 +0530

    Exception Handling Without try catch.

---
+++ Test/src/com/mypack/ExceptionDemo4.java
@@ -0,0 +1,9 @@
+package com.mypack;
+
+public class ExceptionDemo4 {
+	public static void main(String[] args) {
+		System.out.println("Statement 1");
+		System.out.println(10/0);
+		System.out.println("Statement 2");
+	}
+}
