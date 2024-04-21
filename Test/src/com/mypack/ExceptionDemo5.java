537b95443e99f5a547284d922f0bccc5c8221ad6
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-21 21:45:48 +0530

    Exception Handling with try catch block.

--- Test/src/com/mypack/ExceptionDemo5.java
+++ Test/src/com/mypack/ExceptionDemo5.java
@@ -7,8 +7,14 @@ public class ExceptionDemo5 {
 			System.out.println(10/0);
 		}
 		catch(ArithmeticException e) {
-			System.out.println(10/2);
+			System.out.println(10/2);   //--->Normal Termination
 		}
 		System.out.println("Statement2");
 	}
 }
+
+/*
+Statement1
+5
+Statement2 
+ */
