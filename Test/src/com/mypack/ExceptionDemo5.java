372025b5a1afb11d011582c4494609f5a17a29fb
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-21 21:40:53 +0530

    Exception Handling with try catch block.

--- Test/src/com/mypack/ExceptionDemo5.java
+++ Test/src/com/mypack/ExceptionDemo5.java
@@ -3,5 +3,12 @@ package com.mypack;
 public class ExceptionDemo5 {
 	public static void main(String[] args) {
 		System.out.println("Statement1");
+		try {
+			System.out.println(10/0);
+		}
+		catch(ArithmeticException e) {
+			System.out.println(10/2);
+		}
+		System.out.println("Statement2");
 	}
 }
