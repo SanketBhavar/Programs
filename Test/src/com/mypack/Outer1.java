d0889b44bd6001a8817104e7fb42be428c5ec0ce
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-26 22:12:06 +0530

    Accessing inner class code from static area of outer class.

--- Test/src/com/mypack/Outer1.java
+++ Test/src/com/mypack/Outer1.java
@@ -8,8 +8,14 @@ public class Outer1 {
 		}
 	}
 	public static void main(String[] args) {
-		Outer1 o = new Outer1();
-		Outer1.Inner1 i = o.new Inner1();
+//	***Ways of execution***
+//	1st
+//		Outer1 o = new Outer1();
+//		Outer1.Inner1 i = o.new Inner1();
+//		i.methodOne();
+		
+//	2nd
+		Outer1.Inner1 i = new Outer1().new Inner1();
 		i.methodOne();
 	}
 }
