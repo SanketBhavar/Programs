3615b4539454af59906e98a00d57b85eede151bb
SanketBhavar <bhavarsanket2710@gmail.com>
2024-04-29 22:13:58 +0530

    Accessing Inner class code from Outside of Outer class.

--- Test/src/com/mypack/OutsideOuter.java
+++ Test/src/com/mypack/OutsideOuter.java
@@ -3,12 +3,16 @@ package com.mypack;
 class Outer3 {
 	class Inner3 {
 		public void methodOne() {
-			System.out.println("***Inner Class Method***");
+			System.out.println("**Inner Class Method**");
 		}
 	}
 }
+//Accessing Inner class code from Outside of Outer class.
 class OutsideOuter{
 	public static void main(String[] args) {
 		new Outer3().new Inner3().methodOne();
 	}
 }
+/*
+ **Inner Class Method**
+*/
