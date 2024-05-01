b3592e17919d94f22e0f3b32f491edf95412ac21
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-01 22:34:51 +0530

    this Varaible scope in Outer-Inner classes.

--- Test/src/com/mypack/Outer5.java
+++ Test/src/com/mypack/Outer5.java
@@ -6,10 +6,17 @@ public class Outer5 {
 		int x=100;
 		public void methodOne() {
 			int x=1000;
-			System.out.println(x);
+			System.out.println(x);				//Instance variable of methodOne. i.e. 1000
+			System.out.println(this.x);			//current class local variable. i.e.100
+			System.out.println(Outer5.this.x);	//Outer class local variable. i.e. 10
 		}
 	}
 	public static void main(String[] args) {
-		
+		new Outer5().new Inner5().methodOne();
 	}
 }
+/*
+1000
+100
+10
+*/
