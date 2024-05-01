e65f59fb62c64de98c119efbf4ed53a7d6f7a419
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-01 22:29:08 +0530

    Defining main method to execute inner class method.

--- Test/src/com/mypack/Outer5.java
+++ Test/src/com/mypack/Outer5.java
@@ -5,7 +5,11 @@ public class Outer5 {
 	class Inner5 {
 		int x=100;
 		public void methodOne() {
-			
+			int x=1000;
+			System.out.println(x);
 		}
+	}
+	public static void main(String[] args) {
+		
 	}
 }
