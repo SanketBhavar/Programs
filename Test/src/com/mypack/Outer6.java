51cf7cc7cbebf82ba324bb8e54bc85f881d97b63
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-02 21:24:15 +0530

    Defining sum method in inner class.

--- Test/src/com/mypack/Outer6.java
+++ Test/src/com/mypack/Outer6.java
@@ -3,7 +3,9 @@ package com.mypack;
 public class Outer6 {
 	public void methodOne() {
 		class Inner6 {
-			
+			public void sum(int x, int y) {
+				System.out.println("The sum: "+(x+y));
+			}
 		}
 	}
 }
