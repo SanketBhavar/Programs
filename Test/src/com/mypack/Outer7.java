b866b5710c58bd1de23ef41a503a231de5c53be2
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-03 21:37:42 +0530

    Accessing or Printing outer class variables in inner class.

--- Test/src/com/mypack/Outer7.java
+++ Test/src/com/mypack/Outer7.java
@@ -2,10 +2,16 @@ package com.mypack;
 
 public class Outer7 {
 	
+	int x=10;
+	static int y=20;
 	public void methodOne()
 	{
 		class Inner7 {
-			
+			public void methodTwo()
+			{
+				System.out.println(x);
+				System.out.println(y);
+			}
 		}
 	}
 }
