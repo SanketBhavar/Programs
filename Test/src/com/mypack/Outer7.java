467495e2a6ba581bd65352867292122919127ed0
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-03 21:56:13 +0530

    Declaring Inner class inside instance method and Access static and non-static methods.

--- Test/src/com/mypack/Outer7.java
+++ Test/src/com/mypack/Outer7.java
@@ -1,7 +1,7 @@
 package com.mypack;
 
 public class Outer7 {
-	
+	//Declaring Inner class inside instance method then we can access both static and non static members of outer class directly.
 	int x=10;
 	static int y=20;
 	public void methodOne()
@@ -9,9 +9,19 @@ public class Outer7 {
 		class Inner7 {
 			public void methodTwo()
 			{
-				System.out.println(x);
-				System.out.println(y);
+				System.out.println("Instance Variables: "+x);	//10
+				System.out.println("Static Variables: "+y);	//20
 			}
 		}
+		Inner7 i = new Inner7();
+		i.methodTwo();
+	}
+	public static void main(String[] args) {
+		new Outer7().methodOne();
 	}
 }
+
+/*
+ 10
+ 20
+*/
