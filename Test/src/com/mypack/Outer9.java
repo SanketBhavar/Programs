1ce6eebe4b54b6f6a135f4913980f6eebadda23e
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-06 21:22:24 +0530

    Anonymous Inner class that extends a class.

--- Test/src/com/mypack/Outer9.java
+++ Test/src/com/mypack/Outer9.java
@@ -3,10 +3,21 @@ package com.mypack;
 class PopCorn {
 	public void taste()
 	{
-		System.out.println("Spicy");
+		System.out.println("Salty");
 	}
 }
 
 public class Outer9 {
-
+	public static void main(String[] args) {
+		PopCorn p = new PopCorn()
+		{
+			public void taste()
+			{
+				System.out.println("Spicy");
+			}
+		};
+		p.taste();	//Spicy
+		PopCorn p1 = new PopCorn();
+		p1.taste();	//Salty
+	}
 }
