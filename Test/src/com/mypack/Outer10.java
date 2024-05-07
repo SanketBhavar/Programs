0587ef67e715f76378980306f338bbdc54670c6b
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-07 21:31:58 +0530

    Inside Anonymous inner classes declare new method.

---
+++ Test/src/com/mypack/Outer10.java
@@ -0,0 +1,29 @@
+package com.mypack;
+
+class PopCorn1 {
+	public void taste() 
+	{
+		System.out.println("Spicy");
+	}
+}
+
+public class Outer10 {
+	public static void main(String[] args) {
+		PopCorn1 p = new PopCorn1()
+		{
+			public void taste()
+			{
+				methodOne();
+				System.out.println("Salty");
+			}
+			public void methodOne()
+			{
+				System.out.println("Child Specific method");
+			}
+		};
+		//p.methodOne();
+		p.taste();
+		PopCorn1 p1 = new PopCorn1();
+		p1.taste();
+	}
+}
