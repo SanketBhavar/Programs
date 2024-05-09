4a582bc9ec9ef067fd456b3648b213f98abf276d
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-09 20:40:41 +0530

    New Method Inside Anonymous inner classes.

--- Test/src/com/mypack/Outer11.java
+++ Test/src/com/mypack/Outer11.java
@@ -2,6 +2,20 @@ package com.mypack;
 
 public class Outer11 {
 	public static void main(String[] args) {
-		
+		Thread t = new Thread()
+		{
+			public void run()
+			{
+				for(int i=0; i<10; i++)
+				{
+					System.out.println("Child Thread");
+				}
+			}
+		};
+		t.start();
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Main Thread");
+		}
 	}
 }
