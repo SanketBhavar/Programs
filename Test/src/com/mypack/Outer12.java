4030f0e8685fab5de9c943eaaa40316fe1cbbe37
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-11 20:47:39 +0530

    Anonymous Inner Class that Implements an Interface.

--- Test/src/com/mypack/Outer12.java
+++ Test/src/com/mypack/Outer12.java
@@ -4,7 +4,13 @@ public class Outer12 {
 	public static void main(String[] args) {
 		Runnable r = new Runnable()
 		{
-			
+			public void run()
+			{
+				for(int i=0;i<10;i++)
+				{
+					System.out.println("Child Thread");
+				}
+			}
 		};
 	}
 }
