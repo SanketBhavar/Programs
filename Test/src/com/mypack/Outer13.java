5ac6f0fb6dba7bbaec715d1b0244f09c4254deeb
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-13 20:02:48 +0530

    Anonymous Inner Class that define inside Method Arguments.

--- Test/src/com/mypack/Outer13.java
+++ Test/src/com/mypack/Outer13.java
@@ -5,7 +5,13 @@ public class Outer13 {
 		new Thread(
 				new Runnable()
 				{
-					
+					public void run()
+					{
+						for(int i=0;i<10;i++)
+						{
+							System.out.println("Child Thread");
+						}
+					}
 				}
 				).start();
 	}
