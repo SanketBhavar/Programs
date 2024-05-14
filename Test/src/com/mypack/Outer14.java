a093e7805d6c805548d2ce66945d905a94e37480
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-14 21:37:42 +0530

    Static Nested Classes.

--- Test/src/com/mypack/Outer14.java
+++ Test/src/com/mypack/Outer14.java
@@ -2,6 +2,13 @@ package com.mypack;
 
 public class Outer14 {
 	static class Nested {
-		
+		public void methodOne()
+		{
+			System.out.println("Nested Class Method.");
+		}
+	}
+	public static void main(String[] args) {
+		Outer14.Nested n = new Outer14.Nested();
+		n.methodOne();
 	}
 }
