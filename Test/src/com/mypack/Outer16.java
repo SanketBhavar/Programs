3581463e572ef317bcca342974b03b3f0fc7f010
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-17 21:18:39 +0530

    Non-Static variable x cannot be referenced from a static context.

--- Test/src/com/mypack/Outer16.java
+++ Test/src/com/mypack/Outer16.java
@@ -6,8 +6,15 @@ public class Outer16 {
 	static class Nested {
 		public void methodOne()
 		{
-			System.out.println(x);
+			//System.out.println(x); //Cannot make a static reference to the non-static field x
 			System.out.println(y);
 		}
 	}
+	public static void main(String[] args) {
+		Outer16.Nested n = new Outer16.Nested();
+		n.methodOne();
+	}
 }
+/*
+20
+*/
