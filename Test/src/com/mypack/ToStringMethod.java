1a4fb4fb306d92d0b14a98b81579923f5545a0e5
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-19 21:10:45 +0530

    java.lang Package- toString method.

--- Test/src/com/mypack/ToStringMethod.java
+++ Test/src/com/mypack/ToStringMethod.java
@@ -8,4 +8,12 @@ public class ToStringMethod {
 		this.name=name;
 		this.rollno=rollno;
 	}
+	
+	public static void main(String[] args) {
+		ToStringMethod t1 = new ToStringMethod("Sanket", 101);
+		ToStringMethod t2 = new ToStringMethod("Rahul", 102);
+		System.out.println(t1);
+		System.out.println(t1.toString());
+		System.out.println(t2);
+	}
 }
