1baa8db4812e8b281102c0de3917f5d4b50acac3
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-20 22:25:30 +0530

    java.lang Package- toString Method.

--- Test/src/com/mypack/ToStringMethod1.java
+++ Test/src/com/mypack/ToStringMethod1.java
@@ -5,4 +5,17 @@ public class ToStringMethod1 {
 	{
 		return "ToStringMethod1";
 	}
+	public static void main(String[] args) {
+		Integer i = Integer.valueOf(10);
+		String s = new String("Sanket");
+		ToStringMethod1 t = new ToStringMethod1();
+		System.out.println(i);
+		System.out.println(s);
+		System.out.println(t);
+	}
 }
+/*
+10
+Sanket
+ToStringMethod1
+*/
