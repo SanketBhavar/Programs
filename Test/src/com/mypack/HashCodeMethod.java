ff760be799c4aeaf9631ab918daa11c4a06ed4c2
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-21 22:06:50 +0530

    hashCode() Method.

--- Test/src/com/mypack/HashCodeMethod.java
+++ Test/src/com/mypack/HashCodeMethod.java
@@ -8,9 +8,15 @@ public class HashCodeMethod {
 	}
 	public static void main(String[] args)
 	{
+		// in this case, Object class toString() method got executed which is internally calls object class hashcode() method.
+		
 		HashCodeMethod h = new HashCodeMethod(10);
 		HashCodeMethod h1 = new HashCodeMethod(100);
 		System.out.println(h);
 		System.out.println(h1);
 	}
 }
+/*
+com.mypack.HashCodeMethod@515f550a
+com.mypack.HashCodeMethod@cac736f
+*/
