316bb719211b8559f656a3e0783744c79e7bb9fb
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-22 21:33:09 +0530

    Printing parameterized constructor by passing integer values.

--- Test/src/com/mypack/HashCodeMethod1.java
+++ Test/src/com/mypack/HashCodeMethod1.java
@@ -8,4 +8,11 @@ public class HashCodeMethod1 {
 	public int hashCode()	{
 		return i;
 	}
+	public static void main(String[] args) {
+		HashCodeMethod1 h1 = new HashCodeMethod1(10);
+		HashCodeMethod1 h2 = new HashCodeMethod1(100);
+		
+		System.out.println(h1);
+		System.out.println(h2);
+	}
 }
