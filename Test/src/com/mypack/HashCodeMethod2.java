cbefb168d319e1dc0c61f617410415d47de36172
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-24 21:11:41 +0530

    toString() and hashCode() Methods.

--- Test/src/com/mypack/HashCodeMethod2.java
+++ Test/src/com/mypack/HashCodeMethod2.java
@@ -13,5 +13,15 @@ public class HashCodeMethod2 {
 	}
 	public static void main(String[] args) {
 		
+		//For correct output we need to use both toString() and hashCode() Methods.
+		HashCodeMethod2 h1 = new HashCodeMethod2(10);
+		HashCodeMethod2 h2 = new HashCodeMethod2(100);
+		
+		System.out.println(h1);
+		System.out.println(h2);
 	}
 }
+/*
+10
+100
+*/
