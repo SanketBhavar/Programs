b2afe154926d211e8bd4c1af0e6a185ddf6d45b3
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-25 22:20:47 +0530

    equals Method.

--- Test/src/com/mypack/Student.java
+++ Test/src/com/mypack/Student.java
@@ -8,4 +8,14 @@ public class Student {
 		this.name=name;
 		this.rollno=rollno;
 	}
+	public static void main(String[] args) {
+		Student s1 = new Student("Sanket",101);
+		Student s2 = new Student("Rahul",102);
+		Student s3 = new Student("Sanket",101);
+		Student s4 = s1;
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s3));
+		System.out.println(s1.equals(s4));
+	}
 }
