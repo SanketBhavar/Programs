f1c57e11898bc3d421439e348512d2a09eb25db9
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-26 21:04:49 +0530

    .equals() method.

---
+++ Test/src/com/mypack/Student1.java
@@ -0,0 +1,17 @@
+package com.mypack;
+
+public class Student1 {
+	String name;
+	int rollno;
+	Student1(String name, int rollno)
+	{
+		this.name=name;
+		this.rollno=rollno;
+	}
+	public boolean equals(Object obj)
+	{
+		try {
+			
+		}
+	}
+}
