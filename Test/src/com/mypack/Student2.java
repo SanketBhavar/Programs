d97a6c247c702624be6b1370397c95cf061a1738
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-28 21:42:59 +0530

    Simplified version of .equals() method.

--- Test/src/com/mypack/Student2.java
+++ Test/src/com/mypack/Student2.java
@@ -11,12 +11,9 @@ public class Student2 {
 	public boolean equals(Object obj)
 	{
 		try {
-			String name1=this.name;
-			int rollno1=this.rollno;
-			Student2 s2=(Student2)obj;
-			String name2=s2.name;
-			int rollno2=s2.rollno;
-			if(name1.equals(name2) && rollno1==rollno2)
+			Student2 s2 = (Student2)obj;
+			
+			if(name.equals(s2.name) && rollno==s2.rollno)
 			{
 				return true;
 			}
