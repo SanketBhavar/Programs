1a981b480be9b42d042c2d9d12309d387b5d1a9f
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-30 20:59:09 +0530

    Cloning object using clone() method.

--- Test/src/com/mypack/CloneableDemo.java
+++ Test/src/com/mypack/CloneableDemo.java
@@ -1,5 +1,13 @@
 package com.mypack;
 
-public class CloeableDemo {
-
+public class CloneableDemo implements Cloneable
+{
+	int i=10;
+	int j=20;
+	
+	public static void main(String[] args) throws CloneNotSupportedException
+	{
+		CloneableDemo c1 = new CloneableDemo();
+		CloneableDemo c2 = (CloneableDemo)c1.clone();
+	}
 }
