c126c73731b49477480204e9bff8e14607c50d75
SanketBhavar <bhavarsanket2710@gmail.com>
2024-05-30 21:01:52 +0530

    Clone Object using clone() method.

--- Test/src/com/mypack/CloneableDemo.java
+++ Test/src/com/mypack/CloneableDemo.java
@@ -8,6 +8,14 @@ public class CloneableDemo implements Cloneable
 	public static void main(String[] args) throws CloneNotSupportedException
 	{
 		CloneableDemo c1 = new CloneableDemo();
-		CloneableDemo c2 = (CloneableDemo)c1.clone();
+		CloneableDemo c2 = (CloneableDemo)c1.clone();	//Cloning object
+		c2.i=888;
+		c2.j=999;
+		System.out.println(c1.i +"-----------"+c1.j);
+		System.out.println(c2.i +"----------"+c2.j);
 	}
 }
+/*
+10-----------20
+888----------999
+*/
