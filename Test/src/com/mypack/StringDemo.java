b04630f625ee6195d2d8d51546c9f41d0f4547b7
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-01 21:22:15 +0530

    String Example

--- Test/src/com/mypack/StringDemo.java
+++ Test/src/com/mypack/StringDemo.java
@@ -4,6 +4,12 @@ public class StringDemo {
 	public static void main(String[] args) {
 		String s1 = new String("You cannot change me!");
 		String s2 = new String("You cannot change me!");
-		System.out.println(s1==s2);
+		System.out.println(s1==s2);	//false
+		String s3 = "You cannot change me!";
+		System.out.println(s1==s3);	//false
+		String s4 = "You cannot change me!";
+		System.out.println(s3==s4);	//true
+		String s5 = "You cannot "+"change me!";
+		System.out.println(s3==s5);	//true
 	}
 }
