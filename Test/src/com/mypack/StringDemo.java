66213fc7af7ba8cdfac08735e85c1ef44af21cda
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-01 21:06:50 +0530

    String Example

---
+++ Test/src/com/mypack/StringDemo.java
@@ -0,0 +1,9 @@
+package com.mypack;
+
+public class StringDemo {
+	public static void main(String[] args) {
+		String s1 = new String("You cannot change me!");
+		String s2 = new String("You cannot change me!");
+		System.out.println(s1==s2);
+	}
+}
