ca46356b8c064d5deafcf0a0178e64662b0d3f44
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-03 21:41:26 +0530

    Important methods of string class.

---
+++ Test/src/com/mypack/StringDemo2.java
@@ -0,0 +1,9 @@
+package com.mypack;
+
+public class StringDemo2 {
+	public static void main(String[] args) {
+		String s = "Durga";
+		System.out.println(s.charAt(3));	//g
+		//System.out.println(s.charAt(10));	//java.lang.StringIndexOutOfBoundsException
+	}
+}
