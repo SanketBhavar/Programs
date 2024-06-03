c30be7e91e7bb4e3f347effec5495378c60b6d64
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-03 21:26:15 +0530

    String class constructors.

---
+++ Test/src/com/mypack/StringDemo1.java
@@ -0,0 +1,9 @@
+package com.mypack;
+
+public class StringDemo1 {
+	public static void main(String[] args) {
+		char[] ch = {'a','b','c'};
+		String s = new String(ch);
+		System.out.println(s);	//abc
+	}
+}
