363282d8eaf12cda252a079d25a1de05bedf40f9
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-04 21:30:26 +0530

    substring with begin index method.

--- Test/src/com/mypack/StringDemo2.java
+++ Test/src/com/mypack/StringDemo2.java
@@ -14,5 +14,7 @@ public class StringDemo2 {
 		
 		System.out.println(s.equals("DurgaSoftware"));	//true
 		System.out.println(s.equalsIgnoreCase("DURGASOFTWARE"));	//true
+		
+		System.out.println(s.substring(5));	//Software ----->Begin index
 	}
 }
