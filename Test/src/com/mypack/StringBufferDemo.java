de8787ab61b47e43a3f50d3cb9cfc5c268106507
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-07 21:06:59 +0530

    Important Methods of StringBuffer.

--- Test/src/com/mypack/StringBufferDemo.java
+++ Test/src/com/mypack/StringBufferDemo.java
@@ -1,5 +1,10 @@
 package com.mypack;
 
 public class StringBufferDemo {
-
+	public static void main(String[] args) {
+		StringBuffer sb =  new StringBuffer("Durga");
+		System.out.println(sb.length());	//5
+		System.out.println(sb.capacity());	//21
+		System.out.println(sb.charAt(4));	//a
+	}
 }
