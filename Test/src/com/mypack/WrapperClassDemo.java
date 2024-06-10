16284b567751f78bee13679c24e219e1b21d008a
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-10 20:52:37 +0530

    valueOf() method.

---
+++ Test/src/com/mypack/WrapperClassDemo.java
@@ -0,0 +1,13 @@
+package com.mypack;
+
+public class WrapperClassDemo {
+	public static void main(String[] args) throws Exception
+	{
+		Integer i = Integer.valueOf("10");
+		Double d = Double.valueOf("10.5");
+		Boolean b = Boolean.valueOf("Durga");
+		System.out.println(i);
+		System.out.println(d);
+		System.out.println(b);
+	}
+}
