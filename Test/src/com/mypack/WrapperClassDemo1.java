5774e983eff740bacb2c1458ffc93bfc77a17099
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-15 21:47:22 +0530

    xxxValue() method.

---
+++ Test/src/com/mypack/WrapperClassDemo1.java
@@ -0,0 +1,10 @@
+package com.mypack;
+
+public class WrapperClassDemo1 {
+	public static void main(String[] args) {
+		Integer i = new Integer(130);
+		System.out.println(i.byteValue());
+		System.out.println(i.shortValue());
+		
+	}
+}
