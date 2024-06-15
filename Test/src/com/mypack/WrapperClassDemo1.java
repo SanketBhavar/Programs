ed244ec5772ef4a92e0b11496881de97f4939ed2
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-15 21:54:13 +0530

    xxxValue() method of wrapper class.

--- Test/src/com/mypack/WrapperClassDemo1.java
+++ Test/src/com/mypack/WrapperClassDemo1.java
@@ -2,6 +2,7 @@ package com.mypack;
 
 public class WrapperClassDemo1 {
 	public static void main(String[] args) {
+		@SuppressWarnings("removal")
 		Integer i = new Integer(130);
 		System.out.println(i.byteValue());		//-126
 		System.out.println(i.shortValue());		//130
@@ -10,6 +11,6 @@ public class WrapperClassDemo1 {
 		System.out.println(i.floatValue());		//130.0
 		System.out.println(i.doubleValue());	//130.0
 		
-		
+		//xxxValue() method- To convert wrapper object to primitive.
 	}
 }
