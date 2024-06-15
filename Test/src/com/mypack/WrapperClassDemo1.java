f68672577ff51d0300714675aba3281c1253e9ad
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-15 21:50:22 +0530

    xxxValue method for all primitive data type.

--- Test/src/com/mypack/WrapperClassDemo1.java
+++ Test/src/com/mypack/WrapperClassDemo1.java
@@ -3,10 +3,13 @@ package com.mypack;
 public class WrapperClassDemo1 {
 	public static void main(String[] args) {
 		Integer i = new Integer(130);
-		System.out.println(i.byteValue());	//-126
-		System.out.println(i.shortValue());	//130
-		System.out.println(i.intValue());	//130
-		System.out.println(i.longValue());	//130
+		System.out.println(i.byteValue());		//-126
+		System.out.println(i.shortValue());		//130
+		System.out.println(i.intValue());		//130
+		System.out.println(i.longValue());		//130
+		System.out.println(i.floatValue());		//130.0
+		System.out.println(i.doubleValue());	//130.0
+		
 		
 	}
 }
