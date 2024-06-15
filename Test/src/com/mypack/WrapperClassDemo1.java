eed22b44165ad951c66b240e368d8b24808c8379
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-15 21:48:39 +0530

    xxxValue() method.

--- Test/src/com/mypack/WrapperClassDemo1.java
+++ Test/src/com/mypack/WrapperClassDemo1.java
@@ -3,8 +3,10 @@ package com.mypack;
 public class WrapperClassDemo1 {
 	public static void main(String[] args) {
 		Integer i = new Integer(130);
-		System.out.println(i.byteValue());
-		System.out.println(i.shortValue());
+		System.out.println(i.byteValue());	//-126
+		System.out.println(i.shortValue());	//130
+		System.out.println(i.intValue());	//130
+		System.out.println(i.longValue());	//130
 		
 	}
 }
