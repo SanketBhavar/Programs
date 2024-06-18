0f1a83409601196da8c524c005d068166524f623
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-18 22:13:01 +0530

    toString() method.

--- Test/src/com/mypack/WrapperClassDemo3.java
+++ Test/src/com/mypack/WrapperClassDemo3.java
@@ -4,5 +4,12 @@ public class WrapperClassDemo3 {
 	public static void main(String[] args) {
 		Integer i = Integer.valueOf("10");
 		System.out.println(i); 	//10
+		
+		String s1 = Integer.toString(10);
+		String s2 = Boolean.toString(true);
+		String s3 = Character.toString('a');
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
 	}
 }
