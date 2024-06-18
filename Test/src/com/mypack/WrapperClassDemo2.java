e04109c43245c1d0eb99b70bda43f178e1b6d8d8
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-18 21:59:23 +0530

    parseXxx() method.

--- Test/src/com/mypack/WrapperClassDemo2.java
+++ Test/src/com/mypack/WrapperClassDemo2.java
@@ -2,6 +2,9 @@ package com.mypack;
 
 public class WrapperClassDemo2 {
 	public static void main(String[] args) {
+		int i = Integer.parseInt("10");
+		boolean b = Boolean.parseBoolean("Durga");
+		double d = Double.parseDouble("10.5");
 		
 	}
 }
