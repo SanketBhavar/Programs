4fa6280aad93821d1508b15c807c6a2b279826f2
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-23 21:22:15 +0530

    Serialization Demo.

--- Test/src/com/mypack/SerializableDemo1.java
+++ Test/src/com/mypack/SerializableDemo1.java
@@ -4,10 +4,17 @@ import java.io.*;
 
 class Dog implements Serializable
 {
-	
+	int i=10;
+	int j=20;
 }
+class Cat implements Serializable
+{
+	int i=30;
+	int j=40;
+}
+
 public class SerializableDemo1 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 	}
 }
