510ad76d0e82be2f4b18f898e1730b41fa5277e4
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-24 22:02:25 +0530

    ArrayList Example.

--- Test/src/com/mypack/ArrayListDemo.java
+++ Test/src/com/mypack/ArrayListDemo.java
@@ -1,5 +1,14 @@
 package com.mypack;
 
-public class ArrayListDemo {
+import java.util.ArrayList;
 
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList a = new ArrayList();
+		a.add("A");
+		a.add(10);
+		a.add("A");
+		a.add(null);
+		System.out.println(a);
+	}
 }
