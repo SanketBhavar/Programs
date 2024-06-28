62f0332111a9ae1339d7a6a158932e020874228f
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-28 21:30:20 +0530

    addElement method of Vector.

--- Test/src/com/mypack/VectorDemo.java
+++ Test/src/com/mypack/VectorDemo.java
@@ -6,5 +6,10 @@ public class VectorDemo {
 	public static void main(String[] args) {
 		Vector v = new Vector();
 		System.out.println(v.capacity());	//10
+		for(int i=10;i<=10;i++)
+		{
+			v.addElement(i);
+		}
+		System.out.println(v.capacity()); //10
 	}
 }
