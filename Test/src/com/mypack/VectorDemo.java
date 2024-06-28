9f30ab744180fe79841a04a6310048dc0d82c893
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-28 21:40:04 +0530

    Vector Class Example.

--- Test/src/com/mypack/VectorDemo.java
+++ Test/src/com/mypack/VectorDemo.java
@@ -15,5 +15,12 @@ public class VectorDemo {
 		//One Vector reaches its maximum capacity then a new vector object will be created with double capacity.
 		//i.e "NewCapacity = CurrectCapacity*2"
 		System.out.println(v.capacity()); //20
+		System.out.println(v);	//[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, A]
 	}
 }
+/*
+10
+10
+20
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, A]
+*/
