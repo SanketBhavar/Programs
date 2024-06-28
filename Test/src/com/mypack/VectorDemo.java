ac910bde63a83431932af5b8a2329bce3f54ba24
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-28 21:38:43 +0530

    Increasing vector capacity.

--- Test/src/com/mypack/VectorDemo.java
+++ Test/src/com/mypack/VectorDemo.java
@@ -6,10 +6,14 @@ public class VectorDemo {
 	public static void main(String[] args) {
 		Vector v = new Vector();
 		System.out.println(v.capacity());	//10
-		for(int i=10;i<=10;i++)
+		for(int i=1;i<=10;i++)
 		{
 			v.addElement(i);
 		}
 		System.out.println(v.capacity()); //10
+		v.addElement("A");
+		//One Vector reaches its maximum capacity then a new vector object will be created with double capacity.
+		//i.e "NewCapacity = CurrectCapacity*2"
+		System.out.println(v.capacity()); //20
 	}
 }
