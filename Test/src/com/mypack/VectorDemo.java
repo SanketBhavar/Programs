50abdcf3cf7bf55d48316895f1736333b263af9c
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-28 21:26:35 +0530

    Initial Capacity of vector.

---
+++ Test/src/com/mypack/VectorDemo.java
@@ -0,0 +1,10 @@
+package com.mypack;
+
+import java.util.Vector;
+
+public class VectorDemo {
+	public static void main(String[] args) {
+		Vector v = new Vector();
+		System.out.println(v.capacity());	//10
+	}
+}
