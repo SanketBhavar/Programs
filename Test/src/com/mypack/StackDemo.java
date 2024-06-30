008231c643d10cb994c53a27df77b5f38cbfb60c
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-30 21:55:34 +0530

    Stack Example.

---
+++ Test/src/com/mypack/StackDemo.java
@@ -0,0 +1,13 @@
+package com.mypack;
+
+import java.util.Stack;
+
+public class StackDemo {
+	public static void main(String[] args) {
+		Stack s = new Stack<>();
+		s.push("A");
+		s.push("B");
+		s.push("C");
+		System.out.println(s); 	//[A, B, C]
+	}
+}
