710a210399d2db29abfc6e54f4da48fca8295092
SanketBhavar <bhavarsanket2710@gmail.com>
2024-06-30 21:56:43 +0530

    push method of stack.

--- Test/src/com/mypack/StackDemo.java
+++ Test/src/com/mypack/StackDemo.java
@@ -4,6 +4,10 @@ import java.util.Stack;
 
 public class StackDemo {
 	public static void main(String[] args) {
-		
+		Stack s = new Stack<>();
+		s.push("A");
+		s.push("B");
+		s.push("C");
+		System.out.println(s);
 	}
 }
