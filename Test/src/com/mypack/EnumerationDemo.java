6de1ec70cefd5b7cb555463fd2e29fedeeccf438
SanketBhavar <bhavarsanket2710@gmail.com>
2024-07-01 20:48:23 +0530

    Enumeration Example.

--- Test/src/com/mypack/EnumerationDemo.java
+++ Test/src/com/mypack/EnumerationDemo.java
@@ -18,5 +18,16 @@ public class EnumerationDemo {
 			if(i%2==0)
 				System.out.println(i);		//printing even numbers from vector.
 		}
+		System.out.println(v);		//[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 	}
 }
+/*
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+0
+2
+4
+6
+8
+10
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+ */
