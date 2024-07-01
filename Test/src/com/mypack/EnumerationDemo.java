e224d11e42430917a9b83e460e89287b3d3699a0
SanketBhavar <bhavarsanket2710@gmail.com>
2024-07-01 20:43:33 +0530

    hasMoreElements method of enumeration.

--- Test/src/com/mypack/EnumerationDemo.java
+++ Test/src/com/mypack/EnumerationDemo.java
@@ -1,5 +1,6 @@
 package com.mypack;
 
+import java.util.Enumeration;
 import java.util.Vector;
 
 public class EnumerationDemo {
@@ -10,5 +11,10 @@ public class EnumerationDemo {
 			v.addElement(i);
 		}
 		System.out.println(v);		//[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+		Enumeration e = v.elements();
+		while(e.hasMoreElements())
+		{
+			
+		}
 	}
 }
