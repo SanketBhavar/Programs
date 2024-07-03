9de8a20b8d177e73f9fd93abd8cfbcd43a656e8d
SanketBhavar <bhavarsanket2710@gmail.com>
2024-07-03 21:43:16 +0530

    all three cursors in vector.

---
+++ Test/src/com/mypack/CursorClasses.java
@@ -0,0 +1,15 @@
+package com.mypack;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Vector;
+
+public class CursorClasses {
+	public static void main(String[] args) {
+		Vector v = new Vector<>();
+		Enumeration e = v.elements();
+		Iterator itr = v.iterator();
+		ListIterator litr = v.listIterator();
+	}
+}
