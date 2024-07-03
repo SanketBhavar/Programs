6f104c8bee5d5b18514fbf826497790296184ec1
SanketBhavar <bhavarsanket2710@gmail.com>
2024-07-03 21:24:19 +0530

    Iterator Example.

--- Test/src/com/mypack/IteratorDemo.java
+++ Test/src/com/mypack/IteratorDemo.java
@@ -20,6 +20,16 @@ public class IteratorDemo {
 			else
 				itr.remove();
 		}
-		//System.out.println(a);
+		System.out.println(a);		//[0, 2, 4, 6, 8, 10]
 	}
 }
+/*
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+0
+2
+4
+6
+8
+10
+[0, 2, 4, 6, 8, 10]
+ */
