e6ab2dfe8fdc63e15e7d160b1b4928313c000f5e
SanketBhavar <bhavarsanket2710@gmail.com>
2024-07-03 21:21:47 +0530

    even number using iterator.

--- Test/src/com/mypack/IteratorDemo.java
+++ Test/src/com/mypack/IteratorDemo.java
@@ -1,6 +1,7 @@
 package com.mypack;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class IteratorDemo {
 	public static void main(String[] args) {
@@ -9,5 +10,16 @@ public class IteratorDemo {
 		{
 			a.add(i);
 		}
+		System.out.println(a); 		//[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+		Iterator itr = a.iterator();
+		while(itr.hasNext())
+		{
+			Integer i = (Integer)itr.next();
+			if(i%2==0)
+				System.out.println(i); 		//0, 2, 4, 6, 8, 10
+			else
+				itr.remove();
+		}
+		//System.out.println(a);
 	}
 }
