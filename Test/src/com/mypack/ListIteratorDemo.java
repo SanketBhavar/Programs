997087209d93650036aa5f06d9396a2d4d7863d3
SanketBhavar <bhavarsanket2710@gmail.com>
2024-07-03 21:29:29 +0530

    adding string objects in LinkedList.

---
+++ Test/src/com/mypack/ListIteratorDemo.java
@@ -0,0 +1,14 @@
+package com.mypack;
+
+import java.util.LinkedList;
+
+public class ListIteratorDemo {
+	public static void main(String[] args) {
+		LinkedList l = new LinkedList();
+		l.add("balakrishna");
+		l.add("venki");
+		l.add("chiru");
+		l.add("nag");
+		System.out.println(l); 		//[balakrishna, venki, chiru, nag]
+	}
+}
