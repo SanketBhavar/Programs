88414fa3cde48a2422639a21cf59191ab601d76e
SanketBhavar <bhavarsanket2710@gmail.com>
2024-07-03 21:14:09 +0530

    Adding elements in ArrayList.

---
+++ Test/src/com/mypack/IteratorDemo.java
@@ -0,0 +1,13 @@
+package com.mypack;
+
+import java.util.ArrayList;
+
+public class IteratorDemo {
+	public static void main(String[] args) {
+		ArrayList a = new ArrayList();
+		for(int i=0;i<=10;i++)
+		{
+			a.add(i);
+		}
+	}
+}
